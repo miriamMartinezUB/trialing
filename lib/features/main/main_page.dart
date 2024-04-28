8fdc867fc99502b7e9b1ff179ccf7499c140c789
Miriam Martinez <miriam.martinez.mc@gmail.com>
2024-04-28 03:21:21 +0200

    fix: export for web

--- lib/features/main/main_page.dart
+++ lib/features/main/main_page.dart
@@ -1,2 +1,2 @@
 export 'package:trialing/features/main/main_page_mobile.dart'
-    if (dart.library.html) 'package:trialing/features/home/main_page_web.dart';
+    if (dart.library.html) 'package:trialing/features/main/main_page_web.dart';
