25905ad122b07b50b6b7c663ebdb297cda8abe99
Miriam Martinez <miriam.martinez.mc@gmail.com>
2024-04-30 05:44:54 +0200

    chore: add toStr in timeofDay

--- lib/utils/time_of_day_extension.dart
+++ lib/utils/time_of_day_extension.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
 
-extension StringExtension on TimeOfDay {
+extension TimeOfDayExtension on TimeOfDay {
   DateTime toDateTimeFromDate(DateTime date) {
     return DateTime(date.year, date.month, date.day, hour, minute);
+  }
+
+  String toStr() {
+    String s = toString();
+    s = s.replaceAll("TimeOfDay", "");
+    s = s.replaceAll("(", "");
+    s = s.replaceAll(")", "");
+    return s;
+  }
+}
+
+extension ParseToTimeFromString on String {
+  TimeOfDay toTimeOfDay() {
+    List<String> parts = split(':');
+    int hour = int.parse(parts[0]);
+    int minute = int.parse(parts[1]);
+    return TimeOfDay(hour: hour, minute: minute);
   }
 }
